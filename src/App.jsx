2619879b68e290cacee9e1adf41b76eb4eaa684b
Abhishek <abhishek.kabi.21@gmail.com>
2025-11-24 18:49:16 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+
+function App() {
+  const jobDetails = [
+    {
+      id: 1,
+      compname: "Aegis BPO",
+      duration: "Sept 2017 to Nov 2017",
+      status: "Absconded",
+      logo: "./aegis.png",
+    },
+    {
+      id: 2,
+      compname: "DXC Technology",
+      duration: "Feb 2018 to Nov 2018",
+      status: "Absconded",
+      logo: "./dxc.png",
+    },
+    {
+      id: 3,
+      compname: "HP",
+      duration: "Feb 2019 to Mar 2019",
+      status: "Absconded",
+      logo: "./hp.png",
+    },
+    {
+      id: 4,
+      compname: "Quinnox",
+      duration: "Apr 2019 to Jun 2019",
+      status: "Absconded",
+      logo: "./quinnox.png",
+    },
+    {
+      id: 5,
+      compname: "Concentrix",
+      duration: "Aug 2019 to Oct 2019",
+      status: "Exit",
+      logo: "./concentrix.webp",
+    },
+    {
+      id: 6,
+      compname: "Amazon",
+      duration: "Jan 2020",
+      status: "Terminated - Violent behaviour with HR/Recruiter",
+      logo: "./amazon.jpg",
+    },
+    {
+      id: 7,
+      compname: "Teleperformance",
+      duration: "Oct 2020",
+      status: "Absconded",
+      logo: "./tp.svg",
+    },
+    {
+      id: 8,
+      compname: "Cameo Global",
+      duration: "April 2023",
+      status: "Terminated - Violent behaviour with HR/Recruiter",
+      logo: "./newera.png",
+    },
+    {
+      id: 9,
+      compname: "Alorica",
+      duration: "Jan 2025",
+      status: "Absconded - #Hostile text msg to whatsapp group",
+      logo: "./alorica.png",
+    },
+    {
+      id: 10,
+      compname: "Concentrix",
+      duration: "Nov 2025",
+      status: "Absconded - #Hostile text msg to trainer",
+      logo: "./concentrix.webp",
+    },
+  ];
+  return (
+    <>
+      <div className="main-container">
+        {jobDetails.map((details) => (
+          <div className="job-container" key={details.id}>
+            <div className="job-container-details">
+              <div className="heading">
+                <div className="kont">
+                  <p style={{ color: "grey", fontSize: "20px" }}>
+                    {details.id}.
+                  </p>{" "}
+                  <div className="img-comp-logo-div">
+                    <img
+                      src={details.logo}
+                      alt=""
+                      style={{
+                        height: "100%",
+                        width: "100%",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </div>{" "}
+                  <p className="compname">
+                    <strong>{details.compname}</strong>
+                  </p>
+                  <p className="duration" style={{ color: "gray" }}>
+                    {details.duration}
+                  </p>
+                </div>
+              </div>
+              <p className="status">
+                <span style={{ color: "gray" }}>Status:</span>{" "}
+                <span style={{ color: "gray" }}>{details.status}</span>
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
