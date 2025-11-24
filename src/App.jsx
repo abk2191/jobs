4cece73e4967f641ad1e3a99abe48cd61b09a4f2
abk2191 <79880483+abk2191@users.noreply.github.com>
2025-11-25 00:19:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -323,6 +323,7 @@ function App() {
         </div>
         <div className="divider">
           <h1 style={{paddingTop: "50px"}}>Beginning of Phase-2</h1>
+<p style={{fontSize: "14px"}}>The Restart</p>
         </div>
     </>
   );
